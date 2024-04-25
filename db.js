2b029b27a13c5b40f26b65c83d689f2ecdea9fa3
Abdullah Azeez <Software.Abdullah.Azeez@outlook.com>
2024-04-25 16:54:40 -0400

    use local config

--- db.js
+++ db.js
@@ -2,17 +2,17 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
 
-    user: "postgres",
-    // user: "aazeez",
+    // user: "postgres",
+    user: "aazeez",
 
-    host: "localho/st",
-    // host: "dpg-cobhnlmn7f5s73fpi10g-a",
+    // host: "localho/st",
+    host: "dpg-cobhnlmn7f5s73fpi10g-a",
 
-    database: "inventory",
-    // database: "inventory_np51",
+    // database: "inventory",
+    database: "inventory_np51",
 
-    password: "PPU7$$7",
-    // password: "vx17C3g4OLRyPmPomoVUjCdoR6mM44Ov",
+    // password: "PPU7$$7",
+    password: "vx17C3g4OLRyPmPomoVUjCdoR6mM44Ov",
 
     port: 5432,
 });

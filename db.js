d7c18b710d684a37c790558ef6f042024ff89174
Abdullah Azeez <Software.Abdullah.Azeez@outlook.com>
2024-09-02 08:39:14 -0400

    use render config

--- db.js
+++ db.js
@@ -2,17 +2,17 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
 
-    user: "postgres",
-    // user: "aazeez",
+    // user: "postgres",
+    user: "aazeez",
 
-    host: "localhost",
-    // host: "dpg-craqg0lds78s73d8hc9g-a",
+    // host: "localhost",
+    host: "dpg-craqg0lds78s73d8hc9g-a",
 
-    database: "inventory",
-    // database: "inventory_zd19",
+    // database: "inventory",
+    database: "inventory_zd19",
 
-    password: "PPU7$$7",
-    // password: "Hyl6MwIev4Jl1pR344OX3lqBUR0Zgbwf",
+    // password: "PPU7$$7",
+    password: "Hyl6MwIev4Jl1pR344OX3lqBUR0Zgbwf",
 
     port: 5432,
 });

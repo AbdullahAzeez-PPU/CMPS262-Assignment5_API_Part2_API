b055e32da99208bff5ea85238356abc901ca5637
Abdullah Azeez <Software.Abdullah.Azeez@outlook.com>
2024-09-02 08:29:49 -0400

    upgrade render membership, use render config

--- db.js
+++ db.js
@@ -2,17 +2,17 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
 
-    // user: "postgres",
-    user: "aazeez",
+    user: "postgres",
+    // user: "aazeez",
 
-    // host: "localhost",
-    host: "dpg-cobhnlmn7f5s73fpi10g-a",
+    host: "localhost",
+    // host: "dpg-craqg0lds78s73d8hc9g-a",
 
-    // database: "inventory",
-    database: "inventory_np51",
+    database: "inventory",
+    // database: "inventory_zd19",
 
-    // password: "PPU7$$7",
-    password: "vx17C3g4OLRyPmPomoVUjCdoR6mM44Ov",
+    password: "PPU7$$7",
+    // password: "Hyl6MwIev4Jl1pR344OX3lqBUR0Zgbwf",
 
     port: 5432,
 });
